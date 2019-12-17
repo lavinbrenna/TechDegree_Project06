0ec72ea77b137d25a759664ff2b170a02e80f84e
lavinbrenna <53067932+lavinbrenna@users.noreply.github.com>
2019-12-17 08:30:46 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const data = require('../data/data.json').projects;
+
+app.set('view engine', 'pug'); 
+app.use('/static', express.static('public'));
+
+const mainRoutes = require('.routes/index');
+const aboutRoutes = require('.routes/about');
+const projectRoutes = require('.routes/project');
+
+app.use(mainRoutes);
+app.use('/about', aboutRoutes);
+app.use('/project', projectRoutes);
+
+
+app.use((err,req,res,next)=>{
+    res.locals.error = err;
+    res.status(500);
+    res.render('error');
+});
+
+app.listen(port, ()=>
+    console.log(`The application is running on local host ${port}!`)
+);
