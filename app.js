ac526aa6848cc989c64a3c12703a64ed348a6fd5
lavinbrenna <53067932+lavinbrenna@users.noreply.github.com>
2019-12-18 12:33:09 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const data = require('../data/data.json').projects;
+const data = require('./data/data.json').projects;
 
 app.set('view engine', 'pug'); 
 app.use('/static', express.static('public'));
+app.use('/images',express.static('images'));
 
-const mainRoutes = require('.routes/index');
-const aboutRoutes = require('.routes/about');
-const projectRoutes = require('.routes/project');
+const mainRoutes = require('./routes/index');
+const aboutRoutes = require('./routes/about');
+const projectRoutes = require('./routes/project');
 
 app.use(mainRoutes);
 app.use('/about', aboutRoutes);
@@ -18,7 +19,7 @@ app.use('/project', projectRoutes);
 app.use((err,req,res,next)=>{
     res.locals.error = err;
     res.status(500);
-    res.render('error');
+    res.render('errors');
 });
 
 app.listen(port, ()=>
