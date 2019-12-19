b7e6e3eef308082b128e60bd4ebb0ca409946c8e
lavinbrenna <53067932+lavinbrenna@users.noreply.github.com>
2019-12-19 09:48:44 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { data }= require('../data/data.json');
-const { projects } = { data }.projects;
+const { projects } = data;
 
+router.get('/',(req,res)=>{
+    res.redirect('index');
+})
 
 router.get('/index',(req,res)=>{
-    console.log({ projects });
+    console.log(projects);
     res.render('index',{ projects });
     
 });
+
 router.get('about', (req,res)=>{
     res.render('about');
-})
+});
 module.exports = router;
--- routes/project.js
+++ routes/project.js
@@ -1,20 +1,24 @@
 const express = require ('express');
 const router = express.Router();
-const { projects } = require('../data/data.json');
+const { data } = require('../data/data.json');
+const projects = data.projects;
 
-router.get('/project/:id', (req,res)=>{
+
+
+router.get('/:id', (req,res)=>{
 
     const { id } = req.params;
-    const { project_name } = projects[id];
-    const { description } = projects[id];
-    const { technologies } = projects[id];
-    const { live_link } = projects[id];
-    const { github_link } = projects[id];
-    const { image_urls } = projects[id];
-    const projectTemplate = { id, project_name, description, technologies, live_link, github_link, image_urls};
+    const { project_name, description, technologies, live_link, github_link, image_urls } = projects[id];
+ 
+    const projectTemplate = { id, project_name, description, technologies, live_link, github_link, image_urls };
     res.render('project', projectTemplate);
   });
-  
-  
+
+  router.get('/index', (req,res)=>{
+    res.render('index');
+});
+router.get('about', (req,res)=>{
+    res.render('about');
+});
   
   module.exports = router;
