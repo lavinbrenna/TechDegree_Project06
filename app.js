8bedc84c7357f8c7c1c26a7ce89bd1781702f25d
lavinbrenna <53067932+lavinbrenna@users.noreply.github.com>
2019-12-20 17:34:59 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const data = require('./data/data.json').projects;
 app.set('view engine', 'pug'); 
 app.use('/static', express.static('public'));
 
+/*sets up routes for pages*/
 const mainRoutes = require('./routes/index');
 const aboutRoutes = require('./routes/about');
 const projectRoutes = require('./routes/project');
@@ -15,12 +16,14 @@ app.use(mainRoutes);
 app.use('/about', aboutRoutes);
 app.use('/project', projectRoutes);
 
+/* error handlers for when the user enters in an incorrect page*/
 app.use((req,res,next)=>{
     const err = new Error("Looks like this page doesn't exist =O");
     err.status = 404;
+    console.log(err);
     next(err);
 });
-
+/* displays the error that is happening the page */
 app.use((err,req,res,next)=>{
     res.locals.error = err;
     res.status(500);
