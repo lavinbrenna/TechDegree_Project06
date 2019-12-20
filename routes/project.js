b59e59138434c262f0954ecfb565b0a8a1fc8d86
lavinbrenna <53067932+lavinbrenna@users.noreply.github.com>
2019-12-20 16:56:58 -0500

    Add files via upload

--- routes/project.js
+++ routes/project.js
@@ -17,7 +17,7 @@ router.get('/:id', (req,res)=>{
   router.get('/index', (req,res)=>{
     res.render('index');
 });
-router.get('about', (req,res)=>{
+router.get('/about', (req,res)=>{
     res.render('about');
 });
   
